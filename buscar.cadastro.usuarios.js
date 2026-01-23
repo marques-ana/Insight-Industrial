c7adcdba460e8985839aa6286652d1436b7d7188
ANA CLAUDIA SOUZA MARQUES <ana_claudia_marques@estudante.sc.senai.br>
2026-01-23 11:15:30 -0300

    alteracao no cadastro de usuario

--- buscar.cadastro.usuarios.js
+++ buscar.cadastro.usuarios.js
@@ -1,13 +1,23 @@
 window.onload = ()=>{
-  buscandoDadosBancada() 
+  buscandarlistausuarios() 
 }
 
 
-function buscandoDadosBancada(){
-    console.log(buscandoDadosBancada)
-    fetch('http://10.77.241.173:1880/smartsense/cadastro')
+function buscandarlistausuarios(){
+    console.log(buscandarlistausuarios)
+    fetch('http://10.77.241.173:1880/smartsense/listausuario')
     .then(res=>res.json())
     .then(data=>{
     console.log(data)
     })
 }
+
+    fetch('http://10.77.241.173:1880/smartsense/usuariosalvo',{
+        method: 'POST',
+        headers:{
+            'Content-type':'Application/json'
+        },
+        body: JSON.stringify(usuario)
+    }).then(data=>{
+    console.log(data)
+    })
