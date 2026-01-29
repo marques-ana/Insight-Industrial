947893b51436cc3d822d9e243485aad274b83884
ANA CLAUDIA SOUZA MARQUES <ana_claudia_marques@estudante.sc.senai.br>
2026-01-29 09:30:13 -0300

    atualizacao busca de usuario

--- buscar.cadastro.usuarios.js
+++ buscar.cadastro.usuarios.js
@@ -1,5 +1,42 @@
+
 window.onload = ()=>{
-  buscandarlistausuarios() 
+    buscandarlistausuarios() 
+    logar()
+}
+
+
+function logar(e){
+    e.preventDefault();
+
+
+    //Busca os inputs do HTML
+    let input_usuario = document.getElementById("usuario");
+    let input_senha = document.getElementById("senha");
+
+    //Tratamento de erros, caso não tiver esses elementos
+    if(!input_usuario || !input_senha){
+        return;
+    }
+
+    console.log(input_usuario)
+
+    //Se chegou até aqui, conseguiu coletar usuário e senha
+    let usuario = input_usuario.value;
+    let senha = input_senha.value;
+
+    //Com o usuário e senha, podemos tentar o login
+    fetch("http://10.77.241.173:1880/smartsense/listausuario",{
+        method:"POST",
+        body:JSON.stringify({usuario,senha})
+    }).then((resposta)=>{
+        console.log(resposta)
+        if(resposta.ok){
+            resposta.json()
+        }
+    }).then((usuario)=>{
+        alert("Olá, usuario");
+    })
+
 }
 
 
@@ -12,12 +49,3 @@ function buscandarlistausuarios(){
     })
 }
 
-    fetch('http://10.77.241.173:1880/smartsense/usuariosalvo',{
-        method: 'POST',
-        headers:{
-            'Content-type':'Application/json'
-        },
-        body: JSON.stringify(usuario)
-    }).then(data=>{
-    console.log(data)
-    })
